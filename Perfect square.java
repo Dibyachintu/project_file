4c22f38a2deb64072d06b63b5e6932416175f13b
Dibyajyoti sahoo <96558067+Dibyachintu@users.noreply.github.com>
2021-12-24 10:09:22 +0530

    Create Perfect square.java

---
+++ Perfect square.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Perfectsquare {
+
+	public static void main(String[] args) {
+		
+		int n,sum=0;
+		System.out.print("Enter the number: ");
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		
+		for(int i=1;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				sum=sum+i;
+			}
+			
+		}
+		if(n==sum)
+		{
+			System.out.print("Perfect square");
+		}
+		else
+		{
+
+			System.out.print("Not Perfect square");
+		}
+	}
+
+}
